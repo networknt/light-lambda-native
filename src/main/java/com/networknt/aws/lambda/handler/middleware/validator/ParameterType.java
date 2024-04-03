25e0cbebbed073720cc4468c4ef9fa38bb105f0c
Steve Hu <stevehu@gmail.com>
2024-04-03 11:05:20 -0400

    fixes #13 add pre-commit hook config file

--- src/main/java/com/networknt/aws/lambda/handler/middleware/validator/ParameterType.java
+++ src/main/java/com/networknt/aws/lambda/handler/middleware/validator/ParameterType.java
@@ -11,17 +11,17 @@ public enum ParameterType {
 	HEADER;
 
 	private static Map<String, ParameterType> lookup = new HashMap<>();
-	
+
 	static {
 		for (ParameterType type: ParameterType.values()) {
 			lookup.put(type.name(), type);
 		}
 	}
-	
+
 	public static ParameterType of(String typeStr) {
 		return lookup.get(StringUtils.trimToEmpty(typeStr).toUpperCase());
 	}
-	
+
 	public static boolean is(String typeStr, ParameterType type) {
 		return type == of(typeStr);
 	}
